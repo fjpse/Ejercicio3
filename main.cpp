88280d4c6ad2389c5ab9842bf0819078790238b6
Javier de <fjdepedro@gmail.com>
2020-06-04 19:37:42 +0000

    Implementación

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
-#include <iostream>
+#include "Cuenta.h"
+#include "CuentaVip.h"
+#include <vector>
 
-int main() {
-  std::cout << "Hello World!\n";
+int main(int argc, char** argv)
+{
+    std::vector<Cuenta*> cuentas;
+    std::vector<Cuenta*>::iterator it;
+    
+    cuentas.push_back(new Cuenta("Jaimito", 10));
+    cuentas.push_back(new CuentaVip("Jorjito", 5));
+    cuentas.push_back(new Cuenta("Juanito"));
+    
+    for(it = cuentas.begin(); it < cuentas.end(); it++)
+    {
+        (*it)->mostrar();
+    }
+    
+    cuentas[0]->retirar(5);
+    cuentas[2]->ingresar(50);
+    cuentas[2]->transferir(20, *cuentas[1]);
+    
+    for(it = cuentas.begin(); it < cuentas.end(); it++)
+    {
+        (*it)->mostrar();
+    }
+    
+    return 0;
 }
